05ed8a459f88da65f90fc7e2d15fa717f8c82b18
Jerguš Kačmár <jergus.kacmar@cloudtalk.io>
2022-04-12 08:12:41 +0200

    perf: conventional changelog

--- .release-it.js
+++ .release-it.js
@@ -1,26 +1,34 @@
 module.exports = {
-	hooks: {
-		/* 'before:init': ['npm test'], */ // no tests yet, this command would fail
-		'after:bump': ['npx auto-changelog -p'],
-	},
-	git: {
-		/* requireBranch: 'main', */
-		commit: true,
-		commitMessage: 'chore(release): ${version}',
-		commitArgs: '',
-		tag: true,
-		tagName: '${version}',
-		tagAnnotation: '${version}',
-		push: true,
-		requireCommits: true,
-		changelog: 'npx auto-changelog --stdout --commit-limit false -u --template ./changelog.hbs',
-	},
-	github: {
-		release: true,
-		releaseName: '${version}',
-		assets: ['dist/*.zip'],
-	},
-	npm: {
-		publish: false,
-	},
+  hooks: {
+    /* 'before:init': ['npm test'], */
+    // no tests yet, this command would fail
+    /* 'after:bump': ['npx auto-changelog -p'], */
+  },
+  git: {
+    /* requireBranch: 'main', */
+    commit: true,
+    commitMessage: "chore(release): ${version}",
+    commitArgs: "",
+    tag: true,
+    tagName: "${version}",
+    tagAnnotation: "${version}",
+    push: true,
+    requireCommits: true,
+    changelog:
+      "npx auto-changelog --stdout --commit-limit false -u --template ./changelog.hbs",
+  },
+  github: {
+    release: true,
+    releaseName: "${version}",
+    assets: ["dist/*.zip"],
+  },
+  npm: {
+    publish: false,
+  },
+  plugins: {
+    "@release-it/conventional-changelog": {
+      preset: "angular",
+      infile: "CHANGELOG.md",
+    },
+  },
 };
